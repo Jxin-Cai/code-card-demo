f501196af6209eb5d2a085d4e64fa3cef5ee7ef9
“素律” <caijiaxin.cjx@alibaba-inc.com>
2021-05-30 15:52:57 +0800

    init

---
+++ src/main/java/com/jxin/code/demo/article01/cook/domain/aggregation/dishes/acl/event/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 事件层
+ * @author Jxin
+ * @version 1.0
+ * @since 2021/2/10 20:32
+ */
+package com.jxin.code.demo.article01.cook.domain.aggregation.dishes.acl.event;
---
+++ src/main/java/com/jxin/code/demo/article01/cook/domain/aggregation/dishes/factory/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 工厂层
+ * @author Jxin
+ * @version 1.0
+ * @since 2021/2/10 20:32
+ */
+package com.jxin.code.demo.article01.cook.domain.aggregation.dishes.factory;
